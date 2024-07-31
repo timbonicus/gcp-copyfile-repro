8ac036da1d959f440d05d6918bb245a03f95c4a9
Tim Hansen <timbonicush@spotify.com>
2024-07-31 14:14:15 -0600

    Add outputFile example

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,13 @@ WORKDIR /app
 RUN chown node:node /app
 USER node
 
-# Sets the TMPDIR for yarn/node, in case the EPERM error is related to the /tmp directory
-# Unfortunately, the same EPERM error occurs (this time copying from /app/tmp to /app/external)
-#RUN mkdir -p /app/tmp  && chown node:node /app/tmp
-#ENV TMPDIR=/app/tmp
-
 COPY --chown=node:node .yarn .yarn
 COPY --chown=node:node build build
+
+# Sets the TMPDIR for yarn, in case the EPERM error is related to the /tmp directory
+# Unfortunately, the same EPERM error occurs (with the updated path)
+#RUN mkdir -p /app/.yarn/tmp  && chown node:node /app/.yarn/tmp
+#ENV TMPDIR=/app/.yarn/tmp
 
 COPY --chown=node:node .yarnrc.yml package.json express.js build-external.js .
 RUN yarn install
--- build-external.js
+++ build-external.js
@@ -1,7 +1,19 @@
+const { outputFileSync } = require("fs-extra");
 const { spawnSync } = require("child_process");
 
 async function buildExternal() {
+  // Direct write within the mounted volume works
+  outputFileSync(
+    "external/direct-write.txt",
+    "Testing direct write permissions"
+  );
+
+  // cp from the app directory to the mounted volume works
   spawnSync("cp", ["-r", "build/.", "external"], { stdio: "inherit" });
+
+  // yarn add fails with errors like:
+  // Error: negotiator@npm:0.6.3: EPERM: operation not permitted, copyfile '/tmp/xfs-a37ca3f6/archive.zip' -> '/app/external/.cache/negotiator-npm-0.6.3-9d50e36171-8.zip-5b2207c423a44370.tmp'
+  // This also occurs with TMPDIR=/app/tmp in the Dockerfile (but the error reflects the updated path)
   spawnSync("yarn", ["add", "zod"], { cwd: "external", stdio: "inherit" });
 }
 
